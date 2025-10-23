61a73c004268503ebb808c03192bf68ff3c662f9
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 20:07:14 +0300

    Add welcome messages in UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+        System.out.println("Programlama Eglencelidir");
+    }
+}
