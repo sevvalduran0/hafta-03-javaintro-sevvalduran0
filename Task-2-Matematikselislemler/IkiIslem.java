eecfeef7c0e52bcac661ecf83bcb20364b158e69
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 21:15:07 +0300

    Add IkiIslem.java for mathematical calculations

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double pay = 9.5 * 4.5 - 2.5 * 3;
+        double payda = 45.5 - 3.5;
+        double sonuc = pay / payda;
+
+        System.out.printf("%.4f\n", sonuc);
+    }
+}
