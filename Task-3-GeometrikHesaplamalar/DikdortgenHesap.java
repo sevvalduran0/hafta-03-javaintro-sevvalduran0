1cd81a4ac19064a1dd4c6246c8e64f533f99c38f
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 21:20:16 +0300

    Add Rectangle class for area and perimeter calculation

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+    }
+}
