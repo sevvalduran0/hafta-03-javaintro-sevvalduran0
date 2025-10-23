9fbe43a958b5bbdd24ad09c0cf1b7f060b85e48f
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 22:07:06 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,10 @@
+public class CarpımTablosu {
+    public static void main(String[] args) {
+        int sayi = 2;
+
+        System.out.println(sayi + " Çarpım Tablosu:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(sayi + " x " + i + " = " + (sayi * i));
+        }
+    }
+}
