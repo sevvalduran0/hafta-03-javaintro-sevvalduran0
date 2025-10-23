f1bef8454c25184d431e6716f1de7650d8049c11
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 21:16:18 +0300

    Add ToplamaSerisi class to calculate sum of 1 to 9

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,12 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        int toplam = 0;
+        
+        for (int i = 1; i <= 9; i++) {
+            toplam += i;
+        }
+        
+        System.out.println("Toplam: " + toplam);
+    }
+}
+
