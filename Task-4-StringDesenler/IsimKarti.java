21360fd16dc4ad959a085a1e6e22898514bba67c
sevvalduran0 <240541088@firat.edu.tr>
2025-10-23 21:57:49 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String adSoyad = "Sevval Duran";
+        String ogrNo = "240541088";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println("=====================================");
+        System.out.println("           OGRENCI KIMLIK KARTI      ");
+        System.out.println("=====================================");
+        System.out.println(" Ad Soyad  : " + adSoyad);
+        System.out.println(" Ogrenci No : " + ogrNo);
+        System.out.println(" Bölüm      : " + bolum);
+        System.out.println("=====================================");
+        System.out.println(" Dönem      : 2025 - 2026");
+        System.out.println("=====================================");
+    }
+}
